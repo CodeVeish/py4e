5c25a062eebd2be33c102a8be05d19241672ba3b
CodeVeish <codeveish@gmail.com>
2021-04-08 17:18:35 -0500

    exercise 8_2 complete

--- ex_08_02/ex_08_02.py
+++ ex_08_02/ex_08_02.py
@@ -2,7 +2,17 @@ han = open('mbox-short.txt')
 
 for line in han:
     line = line.rstrip()
+#    print('Line:',line)
+#    if line == '' :
+#        print('Skip blank')
+#        continue
     words = line.split()
-    if words[0] != 'From' :
+#    print('Words',words)
+    #Guardian Pattern
+#    if len(words) < 3 :
+#        continue
+# the above gp was combined to the below statement
+    if len(words) < 3 or words[0] != 'From' :
+#        print('ignore')
         continue
     print(words[2])
