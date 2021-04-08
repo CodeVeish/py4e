f43793ecbe646a00bb7772e34bef2826afc361f4
CodeVeish <codeveish@gmail.com>
2021-04-08 16:43:21 -0500

    Commit ex_08_01

---
+++ ex_08_01/Middle.py
@@ -0,0 +1,8 @@
+exlist = [1,2,3,4,5]
+#print(exlist)
+len_exlist = len(exlist)
+#print(len_exlist)
+las_exlist = len_exlist - 1
+#print(las_exlist)
+del exlist[1:las_exlist]
+print(exlist)
---
+++ ex_08_01/chop.py
@@ -0,0 +1,10 @@
+exlist = ['Kevin', 'Nati', 'Dutch']
+#exlist.pop()
+len_ex = len(exlist)
+#print(len_ex)
+del_ex = len_ex - 1
+#print(del_ex)
+del exlist[del_ex]
+# del exlist[2]
+del exlist[0]
+print(exlist)
