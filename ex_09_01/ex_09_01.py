2666f5b1e9d0f3da406af356f25ba8bd508f38de
CodeVeish <codeveish@gmail.com>
2021-04-09 16:08:00 -0500

    ex_09_01.py finish

--- ex_09_01/ex_09_01.py
+++ ex_09_01/ex_09_01.py
@@ -2,5 +2,15 @@
 # It doesn't matter what the values are. Then you can use the in operator as a fast way to check
 # whether a string is in the dictionary.
 
+count = 0
+dictionary = dict()
+
 handle = open('words.txt')
-print(handle)
+for line in handle :
+    words = line.split()
+    for word in words :
+        count = count + 1
+        dictionary[word] = count
+            
+print(dictionary)
+
