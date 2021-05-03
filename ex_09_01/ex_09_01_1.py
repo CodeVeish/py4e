4bdbbef13e14798dcfa2ea6b7395f6313a3ff3e7
CodeVeish <codeveish@gmail.com>
2021-05-02 20:25:37 -0500

    commit

--- ex_09_01/ex_09_01_1.py
+++ ex_09_01/ex_09_01_1.py
@@ -2,19 +2,22 @@
 # It doesn't matter what the values are. Then you can use the in operator as a fast way to check
 # whether a string is in the dictionary.
 
-count = 0
-dictionary = dict()
-
+dic = dict()
 handle = open('words.txt')
 for line in handle :
+    line = line.rstrip()
     words = line.split()
     for word in words :
-        count = count + 1
-        dictionary[word] = count
-            
-# print(dictionary)
+        dic[word] = dic.get(word,0) +1
+
+print(dic)
 usercheck = input('What word would you like to find? ')
-if usercheck in dictionary :
-    print(usercheck,dictionary[usercheck])
+if usercheck in dic :
+    print(usercheck,dic[usercheck])
 else :
     print('Value Not Found')
+
+
+
+
+            
