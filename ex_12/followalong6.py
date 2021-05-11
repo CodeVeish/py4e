d27152a5f4f38c272d7c41ac012d055b19f4d7b0
CodeVeish <codeveish@gmail.com>
2021-05-11 15:58:33 -0500

    commit

--- ex_12/followalong6.py
+++ ex_12/followalong6.py
@@ -1,1 +1,14 @@
-import 
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+#print('URL: ',url)
+#print('html: ',html)
+print('parsed html: ',soup)
+#Retrieve all of the anchor tags
+tags = soup('a')
+print('tags: ',tags)
+for tag in tags:
+    print(tag.get('href',None))
